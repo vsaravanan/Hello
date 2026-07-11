475ae4d82065de640ed58abfd90e59dabe08a243
Viswanathan Saravanan <saravanan.resume@gmail.com>
2026-07-12 01:31:52 +0800

    apply removed

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,16 +10,16 @@ source "$remote_dir/common.sh"
 logfile=$(get_caller_script)
 start_log_file $logfile
 
-kubectl delete deployment $module --ignore-not-found
-kubectl delete svc $service --ignore-not-found
-kubectl delete pod -l app=$module
+# kubectl delete deployment $module --ignore-not-found
+# kubectl delete svc $service --ignore-not-found
+# kubectl delete pod -l app=$module
 
 mylog "Save current image as previous tag (for rollback)"
 kubectl get deployment $module -o jsonpath='{.spec.template.spec.containers[0].image}' > $deploy_path/.previous_tag_api 2>/dev/null || echo "none" > $deploy_path/.previous_tag_api
 cat $deploy_path/.previous_tag_api
 
-mylog "Apply $module manifest"
-kubectl apply -f $deploy_path/$module.yaml
+# mylog "Apply $module manifest"
+# kubectl apply -f $deploy_path/$module.yaml
 
 mylog "Roll out latest API image"
 kubectl set image deployment/$module $module="$api_image"
