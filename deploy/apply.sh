40b8695dd7284c08fc1ae56c03e5a95479dfb2aa
Viswanathan Saravanan <saravanan.resume@gmail.com>
2026-07-12 15:42:55 +0800

    replica 0

--- deploy/apply.sh
+++ deploy/apply.sh
@@ -6,3 +6,5 @@ kubectl apply -f /data/java/Hello/deploy/hello-api.yaml
   
 
 kubectl apply -f /data/fe/hello-ui/deploy/hello-ui.yaml
+
+kubectl scale deployment hello-api hello-ui registry --replicas=0
