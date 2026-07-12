f132c3e24570e48af5ad3260e34f606476723df4
Viswanathan Saravanan <saravanan.resume@gmail.com>
2026-07-12 19:43:24 +0800

    removed ./restart.sh registry

--- deploy/apply.sh
+++ deploy/apply.sh
@@ -7,6 +7,14 @@ kubectl apply -f /data/java/Hello/deploy/hello-api.yaml
 
 kubectl apply -f /data/fe/hello-ui/deploy/hello-ui.yaml
 
-kubectl scale deployment hello-api hello-ui registry --replicas=0
+kubectl scale deployment hello-api hello-ui  --replicas=0
 
 kubectl get deploy,svc | grep -E "registry|hello-api|hello-ui" || true
+
+kubectl scale deployment registry --replicas=1
+
+kubectl rollout status deployment/registry --timeout=60s
+
+# 2. Wait for registry to be ready
+echo "⏳ Waiting for registry to be ready..."
+kubectl wait --for=condition=available deployment/registry --timeout=60s
