f326553de2b0b3dee06b8a28fe17e102be74509e
Viswanathan Saravanan <saravanan.resume@gmail.com>
2026-07-14 22:48:26 +0800

    cltest.sh

---
+++ deploy/cltest.sh
@@ -0,0 +1,26 @@
+
+set -exuo pipefail
+
+# If no parameters, restart all
+if [ $# -eq 0 ]; then
+    echo "which app to be cleaned up "
+    exit 0
+fi
+
+module=$1
+
+cleanup_images() {
+    local repo="$1"
+
+    buildah images --format "{{.Name}} {{.Tag}}" |
+    grep "^${repo} " |
+    while read -r name tag; do
+        if [ "$tag" != "latest" ]; then
+            echo "Deleting ${name}:${tag}"
+            # buildah rmi "${name}:${tag}"
+        fi
+    done
+}
+
+buildah images
+cleanup_images $module
