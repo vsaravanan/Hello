8c60904ce92b2f55cf504aba8f689ebc9a79187b
Viswanathan Saravanan <saravanan.resume@gmail.com>
2026-07-15 17:59:01 +0800

    move

--- datadog/datadog.sh
+++ datadog/datadog.sh
@@ -5,3 +5,10 @@ kubectl create secret generic datadog-secret --from-literal api-key=cff5b1a2d1aa
 
 helm upgrade datadog datadog/datadog-agent -f datadog-agent.yaml -n datadog
 
+helm upgrade datadog datadog/datadog \
+  --set datadog.apiKey=cff5b1a2d1aaac00c29a4294e63a0915 \
+  --set datadog.clusterName=saravanjs \
+  --set datadog.hostname=k8master \
+  --set datadog.clusterAgent.enabled=true \
+  --set datadog.clusterAgent.metricsProvider.enabled=true \
+  -n datadog
