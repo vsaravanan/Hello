ca5ed151bd3a921566f6bce7272363e8c37280c9
Viswanathan Saravanan <saravanan.resume@gmail.com>
2026-07-15 17:28:44 +0800

    values.yaml

--- helm/datadog.sh
+++ helm/datadog.sh
@@ -1,3 +1,7 @@
 helm repo add datadog https://helm.datadoghq.com
 helm install datadog-operator datadog/datadog-operator --namespace datadog --create-namespace
 kubectl create secret generic datadog-secret --from-literal api-key=cff5b1a2d1aaac00c29a4294e63a0915 -n datadog
+
+
+helm upgrade datadog datadog/datadog-agent -f datadog-agent.yaml -n datadog
+
