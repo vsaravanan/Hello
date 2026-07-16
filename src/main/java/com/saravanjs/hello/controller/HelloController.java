137e0ffe3d57f2121e8e0de2f6bcbabd18c50a4c
Viswanathan Saravanan <saravanan.resume@gmail.com>
2026-07-16 15:31:06 +0800

    Db url user

--- src/main/java/com/saravanjs/hello/controller/HelloController.java
+++ src/main/java/com/saravanjs/hello/controller/HelloController.java
@@ -17,10 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     public record Message(String details) { }
+    public record Db(String url, String username) { }
 
     @GetMapping("/hello")
     public ResponseEntity<Message> sayHello() {
         Message message = new Message("Testing hello");
         return ResponseEntity.ok(message);
+    }
+
+    @GetMapping("/db")
+    public ResponseEntity<Db> getDb() {
+
+        String url = System.getenv("DATABASE_URL");
+        String user = System.getenv("DATABASE_USER");
+
+        System.out.println(url);
+        System.out.println(user);
+
+        Db db = new Db(url, user);
+        return ResponseEntity.ok(db);
     }
 }
